0ab170935f73f5accb47b99d6b49c9580fc9049f
Nicktrusere <Nicktrusere@gmail.com>
2023-08-08 08:56:37 -0700

    Updating 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" raddit api"""
+""" reddit api"""
 
 import json
 import requests
@@ -15,7 +15,7 @@ def count_words(subreddit, word_list, after="", count=[]):
     request = requests.get(url,
                            params={'after': after},
                            allow_redirects=False,
-                           headers={'user-agent': 'Custom'})
+                           headers={'user-agent':'Custom'})
 
     if request.status_code == 200:
         data = request.json()
